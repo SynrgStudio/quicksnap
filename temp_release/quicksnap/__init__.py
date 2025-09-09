88489a87ae5a49172d8bc0ddb86dbad2808a5581
SynrgStudio <d.naone@hotmail.com>
2025-09-09 03:41:40 -0300

    Edge Case: Added the posibility to use RMB to rotate camera

--- temp_release/quicksnap/__init__.py
+++ temp_release/quicksnap/__init__.py
@@ -7,7 +7,7 @@ bl_info = {
     "name": "QuickSnap",
     "author": "SynrgStudio",
     "blender": (4, 5, 3),
-    "version": (1, 1, 7),
+    "version": (1, 1, 9),
     "category": "3D View",
     "description": "Quickly snap objects/vertices/curve points - Duplicate objects and snap",
     "warning": "",
